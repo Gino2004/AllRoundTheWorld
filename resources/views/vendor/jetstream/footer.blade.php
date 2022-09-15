a667aaa352998e7483f150652823d4e1773f1531
Gino Hanemaaijer <g.hanemaaijer11@gmail.com>
2022-09-15 11:39:08 +0200

    heb footer en nav gemaakt

---
+++ resources/views/vendor/jetstream/footer.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <footer>
+        <h4>Informatie bedrijf:</h4>
+        <table>
+            <tr>
+                <td>AllRoundTheWorld</td>
+            </tr>
+            <tr>
+                <td>Bakkerstraat 21</td>
+            </tr>
+            <tr>
+                <td>07654879</td>
+            </tr>
+        </table>
+        <table>
+            <tr>
+                <td><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Instagram_logo_2022.svg/2048px-Instagram_logo_2022.svg.png" /></td>
+            </tr>
+        </table>
+    </footer>
+</body>
+
+</html>
